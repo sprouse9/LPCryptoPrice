73f17e0ed3a3acbc6c042017ad7761f935a18eea
randy sprouse <randysprouse@randys-imac-2.myfiosgateway.com>
2022-12-04 11:40:14 -0500

    boiler plate code

---
+++ LPCryptoPrice.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import time
+import datetime
+import AppKit
+
+headers = {
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15',
+	'Accept-Language': 'en-us',
+	'Referer': 'https://www.google.com',
+	'Accept-Encoding': 'gzip, deflate, br'
+}
+
+apikey='HTNSPQ164XX8PT6H5JRAMWW7WIBKMQG3YB'
+
+
+
